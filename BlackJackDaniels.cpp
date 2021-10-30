5f9ab5e9c38b604c20ee5d4f96f5a2611a5ad930
Wize <90353790+MrWize@users.noreply.github.com>
2021-10-30 22:57:48 +0300

    Add files via upload

--- BlackJackDaniels.cpp
+++ BlackJackDaniels.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -12,8 +14,7 @@ enum Suits
 
 enum Ranks
 {
-    ACE = 1,
-    TWO,
+    TWO=2,
     THREE,
     FOUR,
     FIVE,
@@ -22,9 +23,10 @@ enum Ranks
     EIGHT,
     NINE,
     TEN,
-    JACK,
-    QUEEN,
-    KING
+    JACK=10,
+    QUEEN=10,
+    KING=10,
+    ACE=11
 };
 
 class Card
@@ -34,19 +36,72 @@ private:
     Ranks rank;
     bool isFaceUp;
 public:
-
+    Card(Suits s, Ranks r, bool f) : suit(s), rank(r), isFaceUp(f) {}
     void Flip()
     {
         isFaceUp = !isFaceUp;
     }
-
     Ranks getValue()
     {
         return rank;
     }
 };
 
+class Hand
+{
+protected:
+    vector<Card*>m_hand;
+public:
+    void Add(Card* new_card)
+    {
+        m_hand.push_back(new_card);
+    }
+    void Clear()
+    {
+        m_hand.clear();
+    }
+    int GetValue()
+    {
+        int count=0;
+        for (Card* card : m_hand)
+        {
+            if((card->getValue()==ACE)&&(count+ACE>21))      //случай туза
+                count += 1;
+            else
+                count += card->getValue();      //если не туз, то прибавляем значение карты
+        }
+        return count;
+    }
+};
+
+class GenericPlayer :public Hand
+{
+private:
+    string m_name;
+public:
+    virtual bool IsHitting(bool is)
+    {
+        if (is == true)
+            return true;
+        else
+            return false;
+    }
+    bool IsBusted()
+    {
+        if (GetValue() > 21)
+            return true;
+        else 
+            return false;
+    }
+    void Bust()
+    {
+        setlocale(0, "");
+        cout << "У игрока " << m_name << " перебор!";
+    }
+};
+
 int main()
 {
+
     return 0;
 }
